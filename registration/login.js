11db1974fe2158ceaf2cb92bc1bd631870550b6d
Nathrada Buasuwan <nathrada.bua@dome.tu.ac.th>
2025-11-06 21:32:48 +0700

    Create JavaScript file for login page

---
+++ registration/login.js
@@ -0,0 +1,7 @@
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginButton = document.getElementById('loginBtn');
+
+loginButton.addEventListener('click', () => {
+    console.log("Clicked");
+});
