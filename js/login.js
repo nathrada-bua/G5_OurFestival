14ab0d230797589108732006f090059a027ed3f2
Nathrada Buasuwan <nathrada.bua@dome.tu.ac.th>
2025-11-08 20:40:07 +0700

    Change background color and add mascot to log in and sign up page

--- js/login.js
+++ js/login.js
@@ -4,6 +4,8 @@ const loginButton = document.getElementById('loginBtn');
 const rememberMe = document.getElementById('checkbox');
 const usernameErrorText = document.getElementById('username-error');
 const passwordErrorText = document.getElementById('password-error');
+const icon = document.getElementById("icon");
+const icons = ["../resources/Project_Mascot_Default.png", "../resources/Project_Mascot_4.png", "../resources/Project_Mascot_5.png"];
 
 function getCookie(name) {
   const cookies = document.cookie.split("; ");
@@ -49,3 +51,9 @@ loginButton.addEventListener('click', (event) => {
     }
 });
 
+// Mascot animation
+let i = 0;
+setInterval(() => {
+    i = (i+1) % icons.length;
+    icon.src = icons[i];
+}, 2000);
--- js/signup.js
+++ js/signup.js
@@ -7,6 +7,8 @@ const usernameErrorText = document.getElementById('username-error');
 const emailErrorText = document.getElementById('email-error');
 const passwordErrorText = document.getElementById('password-error');
 const confirmPasswordErrorText = document.getElementById('confirm-password-error');
+const icon = document.getElementById("icon");
+const icons = ["../resources/Project_Mascot_Default.png", "../resources/Project_Mascot_4.png", "../resources/Project_Mascot_5.png"];
 
 signUpButton.addEventListener("click", (event) => {
     event.preventDefault();
@@ -44,3 +46,10 @@ signUpButton.addEventListener("click", (event) => {
         usernameErrorText.classList.add("hide");
     }
 });
+
+// Mascot animation
+let i = 0;
+setInterval(() => {
+    i = (i+1) % icons.length;
+    icon.src = icons[i];
+}, 2000);
