52468e7f9f10bdb93adab3d4cd86826cd49863ff
Nathrada Buasuwan <nathrada.bua@dome.tu.ac.th>
2025-11-23 21:01:17 +0700

    Delete unused code

--- js/signup.js
+++ js/signup.js
@@ -1,12 +1,3 @@
-const usernameInput = document.getElementById('username');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-const confirmPasswordInput = document.getElementById('confirm-password');
-const signUpButton = document.getElementById('loginBtn');
-const usernameErrorText = document.getElementById('username-error');
-const emailErrorText = document.getElementById('email-error');
-const passwordErrorText = document.getElementById('password-error');
-const confirmPasswordErrorText = document.getElementById('confirm-password-error');
 const icon = document.getElementById("icon");
 const icons = ["../resources/Project_Mascot_Default.png", "../resources/Project_Mascot_4.png", "../resources/Project_Mascot_5.png"];
 
